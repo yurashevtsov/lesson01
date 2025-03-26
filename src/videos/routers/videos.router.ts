37993a445017d92ca90fcfba298bf44badeaa873
yurashevtsov <yura.shevtsov92@gmail.com>
2025-03-26 09:54:28 +0300

    implemented videos get by id route

--- src/videos/routers/videos.router.ts
+++ src/videos/routers/videos.router.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import { db } from "../../db/in-memory.db";
 import { HttpStatus } from "../../core/types/http-statuses";
+import { Video } from "../types/video";
 
 const videosRouter = Router({});
 
@@ -8,7 +9,17 @@ videosRouter
   .get("/", (req: Request, res: Response) => {
     res.status(HttpStatus.Ok).send(db.videos);
   })
-  .get("/:videoId", (req: Request, res: Response) => {})
+  .get("/:videoId", (req: Request, res: Response) => {
+    const id = Number.parseInt(req.params.videoId);
+    const foundVideo = db.videos.find((video) => video.id === id);
+
+    if (!foundVideo) {
+      res.sendStatus(HttpStatus.NotFound);
+      return;
+    }
+
+    res.status(HttpStatus.Ok).send(foundVideo);
+  })
   .post("/", (req: Request, res: Response) => {})
   .put("/:videoId", (req: Request, res: Response) => {})
   .delete("/:videoId", (req: Request, res: Response) => {});
